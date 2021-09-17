efaca72243823cafbcbeb15d0399c0385b638f21
sonasunny09 <90765586+sonasunny09@users.noreply.github.com>
2021-09-18 00:30:03 +0530

    Add files via upload

---
+++ MenuOptions.java
@@ -0,0 +1,28 @@
+package com.lockedme;
+
+public class MenuOptions {
+	public static void printWelcomeScreen(String appName, String developerName) {
+		String companyDetails = String.format("*****************************************************\n"
+				+ "** Welcome to %s.com. \n" + "** This application was developed by %s.\n"
+				+ "*****************************************************\n", appName, developerName);
+		System.out.println(companyDetails);
+
+	}
+
+	public static void displayMenu() {
+		String menu = "\n\n****** Select any option number from below and press Enter ******\n\n"
+				+ "1) Retrieve all files inside \"main\" folder\n" + "2) Display menu for File operations\n"
+				+ "3) Exit program\n";
+		System.out.println(menu);
+
+	}
+
+	public static void displayFileMenuOptions() {
+		String fileMenu = "\n\n****** Select any option number from below and press Enter ******\n\n"
+				+ "1) Add a file to \"main\" folder\n" + "2) Delete a file from \"main\" folder\n"
+				+ "3) Search for a file from \"main\" folder\n" + "4) Show Previous Menu\n" + "5) Exit program\n";
+
+		System.out.println(fileMenu);
+	}
+
+}
